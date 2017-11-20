b5b4287ac2c2c0b0ebe167ae5d243cbc78d191c2
Denys Berkovskyy <berkovskyy@gmail.com>
2017-11-20 16:51:37 +0000

    Add missing copyright header to keyinfo.h and keyinfo.cpp files

--- src/keyinfo.h
+++ src/keyinfo.h
@@ -1,3 +1,19 @@
+/*
+Header file to store key info
+Copyright (C) 2010  John D. Coleman
+
+This file is part of stenc.
+
+stenc is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 #ifndef KEYINFO_H
 #define KEYINFO_H
 #include <string>
